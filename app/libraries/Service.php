536ec9e48863f208dacdf4a85eea874f3b8b0ec2
Aletni <61947327+Aletni@users.noreply.github.com>
2022-01-12 19:54:32 +0100

    Delete app directory

--- app/libraries/Service.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Load the model 
-    class Service {
-        public function model($model) {
-            //Require model file
-            require_once '../app/models/' . $model . '.php';
-            //Instantiate model
-            return new $model();
-        }
